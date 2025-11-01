0902d39970a3f2e5b6a7fd661b4e05292c8adf8b
rubenwardy <rw@rubenwardy.com>
2025-11-01 18:27:32 +0000

    Drop support for docker-compose v1

--- utils/common.sh
+++ utils/common.sh
@@ -2,15 +2,6 @@
 
 set -e
 
-docker_compose_version=$(docker-compose --version --short)
-docker_compose_major=${docker_compose_version:0:1}
-
-if [[ $docker_compose_major == "2" ]]; then
-	sep="-"
-else
-	sep="_"
-fi
-
 container() {
-	echo "contentdb${sep}$1${sep}1"
+	echo "contentdb-$1-1"
 }
