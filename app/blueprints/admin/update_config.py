5c3566b2fefe93be7b7a57f3a8a10ea0a2d39c39
rubenwardy <rw@rubenwardy.com>
2025-11-28 20:55:30 +0000

    Sort git update checker admin page

--- app/blueprints/admin/update_config.py
+++ app/blueprints/admin/update_config.py
@@ -1,10 +1,17 @@
 from flask import render_template
 
 from . import bp
-from app.models import PackageUpdateConfig, Package
+from app.models import db, PackageUpdateConfig, Package, UserRank
+from app.utils import rank_required
 
 
 @bp.route("/admin/update_config/")
+@rank_required(UserRank.APPROVER)
 def update_config():
-	failing_packages = Package.query.filter(Package.update_config.has(PackageUpdateConfig.task_id != None)).all()
+	failing_packages = (db.session.query(Package)
+			.select_from(PackageUpdateConfig)
+			.filter(PackageUpdateConfig.task_id != None)
+			.order_by(PackageUpdateConfig.last_checked_at.desc())
+			.join(PackageUpdateConfig.package)
+			.all())
 	return render_template("admin/update_config.html", failing_packages=failing_packages)
